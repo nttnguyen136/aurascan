6a93bde42c6e6c08ca209a6e8b0f2e0d412b08c9
nttnguyen136 <nttnguyen136@gmail.com>
2024-01-31 07:38:59 +0000

    update

--- src/app/core/helpers/wallet.ts
+++ src/app/core/helpers/wallet.ts
@@ -1,6 +1,6 @@
 export const isCoin98Browser = () => (window as any)?.coin98?.isMobile;
 export const isLeapBrowser = () => (window as any)?.leap?.mode?.includes('mobile');
 export const isKeplrBrowser = () => (window as any)?.keplr?.mode?.includes('mobile');
-export const isMobileBrowser = () => isCoin98Browser() && isLeapBrowser() && isKeplrBrowser();
+export const isMobileBrowser = () => isCoin98Browser() || isLeapBrowser() || isKeplrBrowser();
 
 
